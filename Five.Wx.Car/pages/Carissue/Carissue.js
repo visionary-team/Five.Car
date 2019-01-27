8b025193c3211c578675aec18ec88118f83ffdff
xueruilong <1162803073@qq.com>
2019-01-27 19:36:05 +0800

    对对对
    对对对

---
+++ Five.Wx.Car/pages/Carissue/Carissue.js
@@ -0,0 +1,91 @@
+// pages/Carissue/Carissue.js
+Page({
+  
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    selectPerson: true,
+    firstPerson: '--请选择--',
+    selectArea: false,
+
+  },
+  //点击选择类型
+  clickPerson: function () {
+    var selectPerson = this.data.selectPerson;
+    if (selectPerson == true) {
+      this.setData({
+        selectArea: true,
+        selectPerson: false,
+      })
+    } else {
+      this.setData({
+        selectArea: false,
+        selectPerson: true,
+      })
+    }
+  },
+  //点击切换
+  mySelect: function (e) {
+    this.setData({
+      firstPerson: e.target.dataset.me,
+      selectPerson: true,
+      selectArea: false,
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+     
+  },
+ 
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
