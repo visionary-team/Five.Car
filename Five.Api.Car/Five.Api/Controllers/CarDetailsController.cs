4ca1f3046c39a5eccdf24fcd9481729dbf1659e5
admin <879895353@qq.com>
2019-01-29 10:46:01 +0800

    123

---
+++ Five.Api.Car/Five.Api/Controllers/CarDetailsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Five.Car.IRepository;
+using Five.Car.Repository;
+
+namespace Five.Api.Controllers
+{
+    public class CarDetailsController : ApiController
+    {
+
+    }
+}
