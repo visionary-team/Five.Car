67021d4d8efe9bd859faf0ce5dfbb756fdcb0a55
admin <879895353@qq.com>
2019-02-18 11:52:34 +0800

    123

--- Five.Api.Car/Five.Car.Repository/OrderAdd/Collect.cs
+++ Five.Api.Car/Five.Car.Repository/OrderAdd/Collect.cs
@@ -18,89 +18,35 @@ namespace Five.Car.Repository
         /// 数据库连接字符串
         /// </summary>
         private static string strcon = "Data Source=orcl;User ID=zhubaoliang;pwd=666666";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Usersid"></param>
+        /// <returns></returns>
         public List<Collection> ShowCollection(string Usersid)
         {
             using (IDbConnection conn = new OracleConnection(strcon))
             {
                 string sql = string.Format("select * from Collection a join cardetails b on a.carid=b.id join carcolor c on b.carcolorid=c.id join cartable d on d.id=b.brandid join IMAGE e on b.id=e.carid where userid='{0}'", Usersid);
-                var collection = conn.Query<Collection>(sql).ToList();
-                return collection;
-            }
-        }
-        /// <summary>
-        /// 发布车源
-        /// </summary>
-        /// <param name="details"></param>
-        /// <returns></returns>
-        public int AddCarDetails(CarDetails details)
-        {
-            using (IDbConnection conn = new OracleConnection(strcon))
-            {
-                string sql = string.Format("insert into CarDetails values('{0}','{1}','{2}','{3}','{4}')", details.Brandid, details.Carcolorid, details.Displacement, details.Address, details.Price);
-
-                var idetails = conn.Execute(sql);
-                return idetails;
-            }
-        }
-        /// <summary>
-        /// 显示地址
-        /// </summary>
-        /// <returns></returns>
-        public List<Address> Address()
-        {
-            using (IDbConnection conn = new OracleConnection(strcon))
-            {
-                string sql = "select Id,UName,Phone,Province,Useraddress from Address";
-                var address = conn.Query<Address>(sql).ToList();
-                return address;
-            }
-        }
-        /// <summary>
-        /// 显示车辆详情
-        /// </summary>
-        /// <param name="Usersid"></param>
-        /// <returns></returns>
-        public List<Orders> ShowOrders(string Usersid)
-        {
-            using (IDbConnection conn = new OracleConnection(strcon))
-            {
-                string sql = string.Format("select * from Collection a join cardetails b on a.carid=b.id join carcolor c on b.carcolorid=c.id join cartable d on d.id=b.brandid join IMAGE e on b.id=e.carid where userid='{0}'", Usersid);
-                var orders = conn.Query<Orders>(sql).ToList();
-                return orders;
+                var a = conn.Query<Collection>(sql).ToList();
+                return a;
             }
         }
 
-        public List<Orders> ShowOrders()
+        public int AddCarDetails(CarDetails c)
+        {
+            using (IDbConnection conn = new OracleConnection(strcon))
+            {
+                string sql = string.Format("insert into CarDetails values('{0}','{1}','{2}','{3}','{4}')", c.Carcolorid, c.Displacement, c.Address, c.Price, c.Imgurl);
+                int i = conn.Execute(sql);
+                return i;
+            }
+        }
+
+        public int Add(CarDetails carDetails)
         {
             throw new NotImplementedException();
-        }
-        /// <summary>
-        /// 车辆详情下拉
-        /// </summary>
-        /// <param name=""></param>
-        /// <returns></returns>
-        public List<CarTable> Details()
-        {
-            using (IDbConnection conn = new OracleConnection(strcon))
-            {
-                string sql = string.Format("select Id,Carbrand from CarTable");
-                var details = conn.Query<CarTable>(sql).ToList();
-                return details;//123
-            }
-        }
-
-        /// <summary>
-        /// 车辆颜色下拉
-        /// </summary>
-        /// <returns></returns>
-        public List<CarColor> Color()
-        {
-            using (IDbConnection conn = new OracleConnection(strcon))
-            {
-                string sql = string.Format("select Id,Colorname from CarColor");
-                var color = conn.Query<CarColor>(sql).ToList();
-                return color;
-            }
         }
     }
 }
