481a406f8a0f36c18e60a0ac81737d4eada05ba2
xueruilong <1162803073@qq.com>
2019-02-19 19:48:19 +0800

    522

--- Five.Api.Car/Five.Car.Repository/OrderAdd/Collect.cs
+++ Five.Api.Car/Five.Car.Repository/OrderAdd/Collect.cs
@@ -29,7 +29,7 @@ namespace Five.Car.Repository
         }
         /// <summary>
         /// 发布车源
-        /// </summary>
+        /// </summary>  
         /// <param name="details"></param>
         /// <returns></returns>
         public int AddCarDetails(CarDetails details)
@@ -38,7 +38,7 @@ namespace Five.Car.Repository
             {
                 string sql = string.Format("insert into cardetails(brandid,carcolorid,displacement,seat,address,details,stock,price,times,iscollection) values('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}',0)", details.Brandid, details.Carcolorid,details.Displacement,details.Seat,details.Address,details.Details,details.Stock,details.Price,details.Times);
 
-                //int i = 1;
+                //int i = 1;1232
 
                 var Idetails = conn.Execute(sql);
                 return Idetails;
